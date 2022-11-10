578469478615121f6efbb7e93f05326b2465dd52
Agustina Nuñez <aguusnm@gmail.com>
2022-11-10 12:59:59 -0300

    Add exercise 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Filter = ({filter, setFilter}) => (
   <div>
@@ -49,10 +50,14 @@ const Persons = ({filter, persons}) => (
 )
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '39-44-5323523' }
-  ])
+  const [persons, setPersons] = useState([])
   const [filter, setFilter] = useState('')
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons').then(response => {
+      setPersons(response.data)
+    })
+  }, [])
 
   return (
     <div>
