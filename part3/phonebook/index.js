371fde697f0ff670332444ff2b3cdf0c9c8077b0
Agustina Nuñez <aguusnm@gmail.com>
2022-11-14 12:20:15 -0300

    Exercises 3.1 to 3.6

---
+++ part3/phonebook/index.js
@@ -0,0 +1,94 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+let persons = [
+    { 
+      name: 'Arto Hellas', 
+      number: '040-123456',
+      id: 1
+    },
+    { 
+        name: 'Ada Lovelace', 
+        number: '39-44-5323523',
+        id: 2
+    },
+    { 
+        name: 'Dan Abramov', 
+        number: '12-43-234345',
+        id: 3
+    },
+    { 
+        name: 'Mary Poppendieck', 
+        number: '39-23-6423122',
+        id: 4
+    }
+]
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
+})
+
+app.get('/info', (req, res) => {
+    const now = new Date()
+    res.send(`
+    <p>Phonebook has info for ${persons.length} people</p>
+    <p>${now}</p>
+    `)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+    const { id } = req.params
+    const person = persons.find(person => person.id === Number(id))
+    
+    if (person) {
+        res.json(person)
+    } else {
+        res.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    const { id } = req.params
+    persons = persons.filter(person => person.id === Number(id))
+    
+    res.status(204).end()
+})
+
+app.post('/api/persons', (req, res) => {
+    const { body } = req
+    if (!body) {
+        return res.status(400).json({
+            error: 'Content missing'
+        })
+    }
+
+    const {name, number} = body
+    if (!name || !number) {
+        return res.status(400).json({
+            error: 'Content missing'
+        })
+    }
+    
+    const nameAlreadyExists = persons.find(person => person.name === name)
+    if (nameAlreadyExists) {
+        return res.status(403).json({
+            error: 'Name must be unique'
+        })
+    }
+    
+    const person = {
+        name,
+        number,
+        id: Math.floor(Math.random() * 1000000)
+    }
+    persons = persons.concat(person)
+    
+    res.json(person)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
