2f5a031f4f4045b9b5082fd94a36f65b9e79e7f1
Agustina Nuñez <aguusnm@gmail.com>
2022-11-17 10:48:54 -0300

    Exercise 3.12

---
+++ part3/phonebook/backend/mongo.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+    console.log('Please provide the password as an argument: node mongo.js <password>')
+    process.exit(1)
+}
+
+const password = process.argv[2]
+const username = 'admin'
+const dbName = 'phonebook'
+
+const url = `mongodb+srv://${username}:${password}@cluster0.pxsre.mongodb.net/${dbName}?retryWrites=true&w=majority`
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+})
+const Person = mongoose.model('Person', personSchema)
+
+mongoose
+    .connect(url)
+    .then(() => console.log('DB connected'))
+    .then(() => {
+        if (process.argv.length < 5) {
+            return Person.find({}).then(persons => {
+                console.log('phonebook:')
+                persons.map(person => console.log(person.name, person.number))
+            })
+        }
+        const newPerson = new Person({
+            name: process.argv[3],
+            number: process.argv[4],
+        })
+        return newPerson.save().then(() => 
+            console.log(`added ${newPerson.name} number ${newPerson.number} to phonebook`)
+        )
+    })
+    .then(() => mongoose.connection.close())
+    .catch(err => console.log(err))
