5e2edb0736c95d83c97e1db9a81d164bf6a19a9f
Agustina Nuñez <aguusnm@gmail.com>
2023-02-22 19:35:23 -0300

    Exercise 4.9, update previous tests in blogs_api

--- part4/blogs/tests/blogs_api.test.js
+++ part4/blogs/tests/blogs_api.test.js
@@ -1,14 +1,38 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
+const Blog = require('../models/blogs')
+const { initialBlogs } = require('./blogs_helper')
 
 const api = supertest(app)
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  Promise.all(initialBlogs.map(async (initBlog) => {
+    const blog = new Blog(initBlog)
+    await blog.save()
+  }))
+})
 
 test('blogs are returned as json', async () => {
   await api
     .get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/)
+})
+
+test('all blogs are returned', async () => {
+  const response = await api.get('/api/blogs')
+  expect(response.body).toHaveLength(initialBlogs.length)
+})
+
+test('all blogs have an id', async () => {
+  const response = await api.get('/api/blogs')
+
+  response.body.map(blog => {
+    expect(blog.id).toBeDefined()
+  })
 })
 
 afterAll(async () => {
---
+++ part4/blogs/tests/blogs_helper.js
@@ -0,0 +1,31 @@
+const initialBlogs = [
+  {
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    likes: 15
+  },
+  {
+    title: "Canonical string reduction",
+    author: "Edsger W. Dijkstra",
+    likes: 121
+  },
+  {
+    title: "Photon",
+    author: "Einstein",
+    likes: 312
+  },
+  {
+    title: 'Relativity',
+    author: 'Einstein',
+    likes: 72
+  },
+  {
+    title: "Physics",
+    author: "Einstein",
+    likes: 128
+  }
+]
+
+module.exports = {
+  initialBlogs
+}
