ecc922235e4e85adf8f51531eb7e4bc47dcd96f4
Agustina Nuñez <aguusnm@gmail.com>
2023-03-08 19:11:07 -0300

    Exercise 4.18 implement token-based authentication

--- part4/blogs/app.js
+++ part4/blogs/app.js
@@ -8,6 +8,7 @@ const logger = require('./utils/logger')
 const middleware = require('./utils/middleware')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 logger.info('Connecting to MongoDB...')
 mongoose.set('strictQuery', false)
@@ -24,6 +25,7 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/login', loginRouter)
 app.use('/api/users', usersRouter)
 
 app.use(middleware.unknownEndpoint)
