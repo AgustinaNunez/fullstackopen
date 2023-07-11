1a9b293a6aa79f3cee97724564b2709414c3c090
Agustina Nuñez <aguusnm@gmail.com>
2023-07-11 18:32:16 -0300

    5.10 sort blog list according to number of likes

--- part5/blogs/src/App.js
+++ part5/blogs/src/App.js
@@ -59,7 +59,7 @@ const App = () => {
       </Togglable>
 
       <br/>
-      {blogs.map(blog =>
+      {blogs.toSorted((a,b) => b.likes - a.likes).map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
     </div>
