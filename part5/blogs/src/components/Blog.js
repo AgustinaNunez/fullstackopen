90585b5a9ba98dfe3b458245994fb88bf573d956
Agustina Nuñez <aguusnm@gmail.com>
2023-07-11 17:44:19 -0300

    5.6 (already done) 5.7 add Blog detail and togglable button

--- part5/blogs/src/components/Blog.js
+++ part5/blogs/src/components/Blog.js
@@ -1,7 +1,28 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({blog}) => {
+  const [visible, setVisible] = useState(false)
+
+  const blogStyle = {
+    padding: 10,
+    border: '1px solid black',
+    marginBottom: 5
+  }
+
+  const toggleVisibility = () => setVisible(!visible)
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} <button onClick={toggleVisibility}>{visible ? 'hide': 'view'}</button>
+      {visible &&
+        <>
+          <p>{blog.url}</p>
+          <p>likes {blog.likes} <button>like</button></p>
+          <p>{blog.author}</p>
+        </>
+      }
+    </div>  
+  )
+}
 
 export default Blog
