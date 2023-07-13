a005cedbcd09eb74c8b85d7617cc12be1a9aa316
Agustina Nuñez <aguusnm@gmail.com>
2023-07-12 23:16:21 -0300

    5.14 Add test for Blog when 'view' button is clicked

--- part5/blogs/src/components/Blog.test.js
+++ part5/blogs/src/components/Blog.test.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import Blog from './Blog'
 
 const buttonsLabel = {
   view: 'view',
   hide: 'hide',
+  like: 'like',
 }
 
 const blog = {
@@ -13,6 +14,7 @@ const blog = {
   title: 'title',
   author: 'author',
   url: '/url',
+  likes: 3,
   user: []
 }
 
@@ -25,4 +27,17 @@ test('renders content with details hidden', () => {
   expect(div).not.toHaveTextContent(buttonsLabel.hide)
   expect(div).not.toHaveTextContent(blog.url)
   expect(div).not.toHaveTextContent(blog.author)
+})
+
+test('renders content detail when view button is clicked', async () => {
+  render(<Blog blog={blog} />)
+  const button = screen.getByText(buttonsLabel.view)
+  await fireEvent.click(button)
+
+  expect(screen.getByText(blog.title)).toBeInTheDocument()
+  expect(screen.getByText(blog.url)).toBeInTheDocument()
+  expect(screen.getByText(blog.author)).toBeInTheDocument()
+  expect(screen.getByText(`likes ${blog.likes}`)).toBeInTheDocument()
+  expect(screen.getByRole('button', { name: buttonsLabel.hide })).toBeInTheDocument()
+  expect(screen.getByRole('button', { name: buttonsLabel.like })).toBeInTheDocument()
 })
