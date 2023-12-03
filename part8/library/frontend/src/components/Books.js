11c083f57e3a0eb00fd8b206419a5017df5cbd47
Agustina Nuñez <aguusnm@gmail.com>
2023-12-03 01:58:41 -0300

    8.21 and 8.22 Some fixes on genre buttons, filter books using graphql queries

--- part8/library/frontend/src/components/Books.js
+++ part8/library/frontend/src/components/Books.js
@@ -4,14 +4,19 @@ import { useState } from "react"
 
 const Books = (props) => {
   const [genre, setGenre] = useState(null)
-  const result = useQuery(QUERY_ALL_BOOKS)
-  const books = result.loading ? [] : result.data?.allBooks
+  const resultFiltered = useQuery(QUERY_ALL_BOOKS, {
+    variables: { genre }
+  })
+  const booksFiltered = resultFiltered.loading ? [] : resultFiltered.data?.allBooks
 
+  const result = useQuery(QUERY_ALL_BOOKS)
+  const allBooks = result.loading ? [] : result.data?.allBooks
+  
   if (!props.show) {
     return null
   }
 
-  const getGenres = () => books
+  const genres = allBooks
     .map(book => book.genres)
     .flat()
     .filter((value, i, self) => self.indexOf(value) === i)
@@ -21,13 +26,13 @@ const Books = (props) => {
       <div style={{display: 'flex', alignItems: 'center', gap: '0.5rem', margin: 0}}>
         <h2>books</h2>{genre && <p>(in genre <b>{genre}</b>)</p>}
       </div> 
-      {getGenres().map(genre =>
+      {genres.map(genre =>
         <button key={genre} onClick={() => setGenre(genre)}>
           {genre}
         </button>
       )}
       <button onClick={() => setGenre(null)}>all genres</button>
-      {books?.length
+      {booksFiltered?.length
         ? <table>
           <tbody>
             <tr>
@@ -35,16 +40,13 @@ const Books = (props) => {
               <th>author</th>
               <th>published</th>
             </tr>
-            {books
-              .filter(b => genre ? b.genres.includes(genre) : true)
-              .map((b) => (
-                <tr key={b.title}>
-                  <td>{b.title}</td>
-                  <td>{b.author.name}</td>
-                  <td>{b.published}</td>
-                </tr>
-              ))
-            }
+            {booksFiltered.map((b) => (
+              <tr key={b.title}>
+                <td>{b.title}</td>
+                <td>{b.author.name}</td>
+                <td>{b.published}</td>
+              </tr>
+            ))}
           </tbody>
         </table>
         : <p>No books founded</p>
