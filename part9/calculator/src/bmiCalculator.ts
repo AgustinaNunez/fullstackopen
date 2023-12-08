ac48fc6e128918f628d33aa24dcc63b89fc60c66
Agustina Nuñez <aguusnm@gmail.com>
2023-12-08 01:00:17 -0300

    9.1-9.2 Setup and add bmiCalculator

---
+++ part9/calculator/src/bmiCalculator.ts
@@ -0,0 +1,33 @@
+const calculateBmi = (height: number, weight: number) => {
+  const bmi = weight / Math.pow(height, 2)
+  
+  if (bmi < 16) return 'Underweight (Severe thinness)'
+  if (bmi < 17) return 'Underweight (Moderate thinness)'
+  if (bmi < 18.5) return 'Underweight (Mild thinness)'
+  if (bmi < 25) return 'Normal (healthy weight)'
+  if (bmi < 30) return 'Obese (Class I)'
+  if (bmi < 35) return 'Obese (Class II)'
+  if (bmi >= 40) return 'Obese (Class III)'
+
+  throw new Error('Invalid values provided')
+}
+
+const parseBmiArgs = (args: string[]) => {
+  if (args.length < 4) throw new Error('Not enough arguments')
+  if (args.length > 4) throw new Error('Too many arguments')
+
+  const height = Number(args[2])
+  const weight = Number(args[3])
+  if (height > 0 && weight > 0) {
+    return { height, weight }
+  }
+  throw new Error('Provide valid numbers for height and weight')
+}
+
+try {
+  const { height, weight } = parseBmiArgs(process.argv)
+  const bmi = calculateBmi(height, weight)
+  console.log(bmi)
+} catch(error) {
+  console.error(`Error: ${error.message}.`)
+}
