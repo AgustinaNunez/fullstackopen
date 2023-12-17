7c92312a04453e4d30de30816a92afda44504960
Agustina Nuñez <aguusnm@gmail.com>
2023-12-17 04:23:22 -0300

    9.22 Define types for OccupationalHealthcareEntry and HospitalEntry

--- part9/patientor/backend/src/types.ts
+++ part9/patientor/backend/src/types.ts
@@ -8,8 +8,6 @@ export interface Diagnose {
   name: string;
   latin?: string;
 }
-export interface Entry {
-}
 export interface Patient {
   id: string;
   name: string;
@@ -19,5 +17,37 @@ export interface Patient {
   occupation: string;
   entries: Entry[];
 }
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3
+}
+interface Discharge {
+  date: string;
+  criteria: string;
+}
+interface SickLeave {
+  startDate: string;
+  endDate: string;
+}
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnose['code']>;
+}
+interface HospitalEntry extends BaseEntry {
+  type: 'Hospital';
+  discharge: Discharge;
+}
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: 'Occupational Healthcare';
+  sickLeave: SickLeave;
+}
 export type NonSensitivePatient = Omit<Patient, 'ssn' | 'entries'>;
 export type NewPatientData = Omit<Patient, 'id'>;
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry;
