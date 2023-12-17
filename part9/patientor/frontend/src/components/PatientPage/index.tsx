ab9b2b09f07f7ccec062cc36cc51722f7c6afd22
Agustina Nuñez <aguusnm@gmail.com>
2023-12-17 05:21:20 -0300

    9.24 Add diagnoses from the /api/diagnoses endpoint

--- part9/patientor/frontend/src/components/PatientPage/index.tsx
+++ part9/patientor/frontend/src/components/PatientPage/index.tsx
@@ -3,19 +3,24 @@ import FemaleIcon from '@mui/icons-material/Female';
 import MaleIcon from '@mui/icons-material/Male';
 import { useEffect, useState } from "react";
 
-import { Gender, Patient } from "../../types";
+import { Diagnosis, Gender, Patient } from "../../types";
 import patientService from '../../services/patients';
+import diagnosisService from '../../services/diagnosis';
 import { Typography } from "@mui/material";
 
 const PatientPage = () => {
   const { id } = useParams();
   const [patient, setPatient] = useState<Patient>();
+  const [diagnosis, setDiagnosis] = useState<Diagnosis[]>();
 
   useEffect(() => {
     if (!id) return;
     patientService
       .getById(id)
       .then(data => setPatient(data));
+    diagnosisService
+      .getAll()
+      .then(data => setDiagnosis(data));
   }, []);
 
   if (!patient) return null;
@@ -32,12 +37,20 @@ const PatientPage = () => {
       <Typography variant="body1">occupation: {patient.occupation}</Typography>
       <Typography variant="h6">entries</Typography>
       {patient.entries?.map(e => 
-        <Typography variant="body1">
-          {e.date} {e.description}
+        <div key={e.id}>
+          <Typography variant="body1">
+            {e.date} {e.description}
+          </Typography>
           <ul>
-            {e.diagnosisCodes?.map(code => code && <li>{code}</li>)}
+            {e.diagnosisCodes?.map((code) => code && 
+              <li key={code}>
+                <Typography variant="body1">
+                  {code} {diagnosis?.find(d => d.code === code)?.name}
+                </Typography>
+              </li>
+            )}
           </ul>
-        </Typography>
+        </div>
       )}
     </div>
   );
